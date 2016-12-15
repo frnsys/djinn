668b19b12d85ef38b5240cd729c31a266d10cead
Francis Tseng <f+accounts@frnsys.com>
2016-12-15 09:36:01 -0500

    initial pass at routing

--- src/actors/actor.rs
+++ src/actors/actor.rs
@@ -1,5 +1,6 @@
+use std::net::SocketAddr;
 use std::sync::{Arc, RwLock};
-use rustc_serialize::{json, Decodable, Encodable};
+use rustc_serialize::{json, Decodable, Encodable, Decoder, Encoder};
 
 pub type Inbox<M> = Arc<RwLock<Vec<M>>>;
 
@@ -33,3 +34,29 @@ pub trait Actor : Send + Sync {
 pub type ActorRef<A: Actor> = Arc<RwLock<Box<A>>>;
 pub type ActorVec<A: Actor> = Vec<ActorRef<A>>;
 pub type ActorVecRef<A: Actor> = Arc<RwLock<ActorVec<A>>>;
+
+pub struct RemoteAddr(SocketAddr);
+
+impl Decodable for RemoteAddr {
+    fn decode<D: Decoder>(d: &mut D) -> Result<RemoteAddr, D::Error> {
+        let s = try!(d.read_str());
+        let addr = s.parse().expect("Unable to parse socket address");
+        Ok(RemoteAddr(addr))
+    }
+}
+
+impl Encodable for RemoteAddr {
+    fn encode<S: Encoder>(&self, s: &mut S) -> Result<(), S::Error> {
+        // TODO seems weird
+        let addr = self.0.to_string();
+        let addr = addr.as_str();
+        s.emit_str(addr)
+    }
+}
+
+// TODO eventually use this to locate actors locally or across a network
+#[derive(RustcDecodable, RustcEncodable)]
+pub enum ActorPath {
+    Local { id: usize },
+    Remote { addr: RemoteAddr, id: usize }
+}
--- src/actors/path.rs
+++
@@ -1,7 +0,0 @@
-use std::net::SocketAddr;
-
-// TODO eventually use this to locate actors locally or across a network
-enum ActorPath {
-    Local { id: usize },
-    Remote { addr: SocketAddr, id: usize }
-}
