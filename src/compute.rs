e98b83b29cbec2268501c1e213a44624b55c4e45
Francis Tseng <f+accounts@frnsys.com>
2017-01-01 16:06:12 -0500

    managers/workers/populations can now take any type that implements Redis::Command, so we can use single Redis clients or Redis Cluster clients

--- src/compute.rs
+++ src/compute.rs
@@ -4,8 +4,8 @@ use std::collections::HashMap;
 use rmp_serialize::decode::Error;
 use rmp_serialize::{Encoder, Decoder};
 use rustc_serialize::{Encodable, Decodable};
-use sim::{Agent, State, Simulation};
-use redis::{Commands, Connection, Client, PipelineCommands, pipe};
+use sim::{Agent, Simulation};
+use redis::{Commands, Client, Connection, PipelineCommands, pipe};
 use uuid::Uuid;
 
 fn decode<R: Decodable>(inp: Vec<u8>) -> Result<R, Error> {
@@ -21,31 +21,16 @@ fn encode<R: Encodable>(data: R) -> Result<Vec<u8>, io::Error> {
     }
 }
 
-fn get_agent<S: State>(id: Uuid, conn: &Connection) -> Option<Agent<S>> {
-    let data = conn.get(id.to_string()).unwrap();
-    Some(Agent {
-        id: id,
-        state: decode(data).unwrap(),
-    })
-}
-
-fn set_agent<S: State>(id: Uuid, state: S, conn: &Connection) {
-    // let data = json::encode(&state).unwrap();
-    let data = encode(&state).unwrap();
-    let _: () = conn.set(id.to_string(), data).unwrap();
-}
-
-/// An interface to the agent population.
-pub struct Population<S: Simulation> {
-    conn: Connection,
+/// An interface to the Redis-backed agent population.
+pub struct Population<S: Simulation, C: Commands> {
+    conn: C,
     simulation: S,
 }
 
-impl<S: Simulation> Population<S> {
-    pub fn new(addr: &str, simulation: S) -> Population<S> {
-        let client = Client::open(addr).unwrap();
+impl<S: Simulation, C: Commands> Population<S, C> {
+    pub fn new(simulation: S, conn: C) -> Population<S, C> {
         Population {
-            conn: client.get_connection().unwrap(),
+            conn: conn,
             simulation: simulation,
         }
     }
@@ -59,15 +44,15 @@ impl<S: Simulation> Population<S> {
         decode(data).unwrap()
     }
 
-    /// Send a String to websocket clients.
-    pub fn ws_emit(&self, message: String) -> () {
-        let _: () = self.conn.publish("ws", message).unwrap();
+    pub fn set_world(&self, world: S::World) {
+        let data = encode(&world).unwrap();
+        let _: () = self.conn.set("world", data).unwrap();
     }
 
     /// Create a new agent with the specified state, returning the new agent's id.
     pub fn spawn(&self, state: S::State) -> Uuid {
         let id = Uuid::new_v4();
-        set_agent(id, state.clone(), &self.conn);
+        self.set_agent(id, state.clone());
         let _: () = self.conn.sadd("population", id.to_string()).unwrap();
         self.simulation.setup(Agent {
                                   id: id,
@@ -78,8 +63,18 @@ impl<S: Simulation> Population<S> {
     }
 
     /// Get an agent by id.
-    pub fn get(&self, id: Uuid) -> Option<Agent<S::State>> {
-        get_agent(id, &self.conn)
+    pub fn get_agent(&self, id: Uuid) -> Option<Agent<S::State>> {
+        let data = self.conn.get(id.to_string()).unwrap();
+        Some(Agent {
+            id: id,
+            state: decode(data).unwrap(),
+        })
+    }
+
+    /// Set an agent state by id.
+    pub fn set_agent(&self, id: Uuid, state: S::State) {
+        let data = encode(&state).unwrap();
+        let _: () = self.conn.set(id.to_string(), data).unwrap();
     }
 
     /// Deletes an agent by id.
@@ -129,43 +124,54 @@ impl<S: Simulation> Population<S> {
             let _: () = self.conn.del(keys).unwrap();
         }
     }
+
+    /// Reset the population.
+    pub fn reset(&self) {
+        // reset sets
+        let _: () = self.conn.del("population").unwrap();
+        self.reset_indices();
+    }
 }
 
-pub struct Manager<S: Simulation> {
+pub struct Manager<S: Simulation, C: Commands> {
     conn: Connection,
-    reporters: HashMap<usize, Box<Fn(&Population<S>, &Connection) -> () + Send>>,
-    pub population: Population<S>,
+    reporters: HashMap<usize, Box<Fn(&Population<S, C>, &Connection) -> () + Send>>,
+    pub population: Population<S, C>,
 }
 
-impl<S: Simulation> Manager<S> {
-    pub fn new(addr: &str, simulation: S, world: S::World) -> Manager<S> {
+impl<S: Simulation, C: Commands> Manager<S, C> {
+    pub fn new(addr: &str, conn: C, simulation: S, world: S::World) -> Manager<S, C> {
+        let population = Population::new(simulation, conn);
+        population.reset();
+        population.set_world(world);
+
+        // conn for commanding workers
         let client = Client::open(addr).unwrap();
         let conn = client.get_connection().unwrap();
 
-        let data = encode(&world).unwrap();
-        let _: () = conn.set("world", data).unwrap();
-
-        // reset sets
-        let _: () = conn.del("workers").unwrap();
-        let _: () = conn.del("finished").unwrap();
-        let _: () = conn.del("population").unwrap();
-        let _: () = conn.del("to_decide").unwrap();
-        let _: () = conn.del("to_update").unwrap();
-
-        let population = Population::new(addr, simulation);
-        population.reset_indices();
-
-        // start with "idle" phase
-        let _: () = conn.set("current_phase", "idle").unwrap();
-
-        Manager {
-            conn: conn,
+        let m = Manager {
             population: population,
             reporters: HashMap::new(),
-        }
+            conn: conn,
+        };
+        m.reset();
+        m
     }
 
-    pub fn start(&self, n_steps: usize) -> () {
+    /// Reset the manager. This unregisters all workers and queues.
+    pub fn reset(&self) {
+        // start with "idle" phase
+        let _: () = self.conn.set("current_phase", "idle").unwrap();
+
+        // reset sets
+        let _: () = self.conn.del("workers").unwrap();
+        let _: () = self.conn.del("finished").unwrap();
+        let _: () = self.conn.del("to_decide").unwrap();
+        let _: () = self.conn.del("to_update").unwrap();
+    }
+
+    /// Run the simulation for `n_steps`.
+    pub fn run(&self, n_steps: usize) -> () {
         let mut steps = 0;
 
         if self.n_workers() == 0 {
@@ -203,7 +209,7 @@ impl<S: Simulation> Manager<S> {
     /// compute aggregate statistics, etc, and a Redis connection
     /// that can be used, for example, to send reports via pubsub.
     pub fn register_reporter<F>(&mut self, n_steps: usize, func: F) -> ()
-        where F: Fn(&Population<S>, &Connection) -> () + Send + 'static
+        where F: Fn(&Population<S, C>, &Connection) -> () + Send + 'static
     {
         self.reporters.insert(n_steps, Box::new(func));
     }
@@ -213,47 +219,48 @@ impl<S: Simulation> Manager<S> {
         }
     }
 
+    /// Get the number of workers.
     pub fn n_workers(&self) -> usize {
         self.conn.scard::<&str, usize>("workers").unwrap()
     }
 }
 
-pub struct Worker<S: Simulation> {
+pub struct Worker<S: Simulation, C: Commands> {
     id: Uuid,
     addr: String,
-    population: Population<S>,
+    population: Population<S, C>,
     simulation: S,
 }
 
-impl<S: Simulation> Worker<S> {
-    pub fn new(addr: &str, simulation: S) -> Worker<S> {
+impl<S: Simulation, C: Commands> Worker<S, C> {
+    pub fn new(addr: &str, conn: C, simulation: S) -> Worker<S, C> {
         Worker {
             id: Uuid::new_v4(),
             addr: addr.to_owned(),
-            population: Population::new(addr, simulation.clone()),
+            population: Population::new(simulation.clone(), conn),
             simulation: simulation,
         }
     }
 
     pub fn start(&self) {
         let client = Client::open(self.addr.deref()).unwrap();
-        let conn = client.get_connection().unwrap();
+        let manager = client.get_connection().unwrap();
 
         // register with the manager
-        let _: () = conn.sadd("workers", self.id.to_string()).unwrap();
+        let _: () = manager.sadd("workers", self.id.to_string()).unwrap();
 
         // subscribe to the command channel
         let mut pubsub = client.get_pubsub().unwrap();
         pubsub.subscribe("command").unwrap();
 
         // check what the current phase is
-        let phase: String = client.get("current_phase").unwrap();
-        self.process_cmd(phase.as_ref(), &conn);
+        let phase: String = manager.get("current_phase").unwrap();
+        self.process_cmd(phase.as_ref(), &manager);
 
         loop {
             let msg = pubsub.get_message().unwrap();
             let payload: String = msg.get_payload().unwrap();
-            self.process_cmd(payload.as_ref(), &conn);
+            self.process_cmd(payload.as_ref(), &manager);
             if payload == "terminate" {
                 break;
             }
@@ -279,14 +286,10 @@ impl<S: Simulation> Worker<S> {
     }
 
     fn decide(&self, simulation: &S, conn: &Connection) {
-        let world: S::World = {
-            let world_data = conn.get("world").unwrap();
-            decode(world_data).unwrap()
-        };
-
+        let world = self.population.world();
         while let Ok(id) = conn.spop::<&str, String>("to_decide") {
             let id = Uuid::parse_str(&id).unwrap();
-            match get_agent::<S::State>(id, &conn) {
+            match self.population.get_agent(id) {
                 Some(agent) => {
                     let updates = simulation.decide(agent, world.clone(), &self.population);
                     let mut rpipe = pipe();
@@ -316,10 +319,10 @@ impl<S: Simulation> Worker<S> {
                 }
             };
             let id = Uuid::parse_str(&id).unwrap();
-            match get_agent::<S::State>(id, &conn) {
+            match self.population.get_agent(id) {
                 Some(agent) => {
                     let new_state = simulation.update(agent.state.clone(), updates);
-                    set_agent(id, new_state, &conn);
+                    self.population.set_agent(id, new_state);
                     let _: () = conn.sadd("to_decide", id.to_string()).unwrap();
                 }
                 None => (),
